57deb8a1eecfb9f0ba537317e4a2afd665b6565b
Dan Bikle <bikle@bikle.com>
2016-12-12 15:33:56 -0800

    mo

--- flask411.py
+++ flask411.py
@@ -20,7 +20,7 @@ def hello():
 
 @application.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
+    return send_from_directory(os.path.join(application.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
                                
 if __name__ == "__main__":
