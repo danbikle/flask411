8bba8794f0241291134f4e11b0ad0b735a833023
Dan Bikle <bikle@bikle.com>
2016-12-11 19:48:27 -0800

    hi

---
+++ f2.py
@@ -0,0 +1,19 @@
+# f2.py
+# ref:
+# http://flask.pocoo.org/
+
+# Demo:
+# export FLASK_APP=f2.py
+# flask run
+# curl 127.0.0.1:5000/
+
+import pdb
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    # pdb.set_trace()
+    return "Hello World!\n"
+
+'bye'
